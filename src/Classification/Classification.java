7e6b5ea5f2a5e43e542dc5ba1201f1d032759aae
JonasM <jonas.moewes02@gmail.com>
2022-11-24 18:43:53 +0100

    Classification funktioniert jetz eiogentlci ganz gut

--- src/Classification/Classification.java
+++ src/Classification/Classification.java
@@ -1,27 +1,30 @@
 package Classification;
 
-import weka.classifiers.Classifier;
+import weka.attributeSelection.Ranker;
 import weka.classifiers.Evaluation;
-import weka.classifiers.evaluation.output.prediction.PlainText;
 import weka.classifiers.meta.FilteredClassifier;
-import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.classifiers.trees.J48;
+import weka.filters.supervised.attribute.AttributeSelection;
+import  weka.attributeSelection.InfoGainAttributeEval;
 import weka.filters.unsupervised.attribute.StringToWordVector;
-
-import java.io.File;
 
 
 public class Classification {
+    private static final int WORDS_TO_KEEP = 1000;
+    private static final int NUM_TO_SELECT = 120;
     static Instances trainDataSet;
     static Instances testDataSet;
     static J48 j48;
+    static AttributeSelection aSFilter;
+    static StringToWordVector stringToWordVector;
 
     static FilteredClassifier fc;
 
     public static void main(String [] args){
         try{
+            //Daten einlesen
             DataSource trainData = new DataSource("Data/TrainDataProcessed.arff");
             trainDataSet = trainData.getDataSet();
             trainDataSet.setClassIndex(1);
@@ -31,40 +34,71 @@ public class Classification {
             testDataSet = testData.getDataSet();
             testDataSet.setClassIndex(1);
 
-            trainDataSet.setClassIndex(trainDataSet.numAttributes()-1);
             j48 = new J48();
 
-            StringToWordVector stringToWordVector = new StringToWordVector();
-            System.out.println(stringToWordVector.isOutputFormatDefined());
-            stringToWordVector.setInputFormat(trainDataSet);
-            stringToWordVector.setLowerCaseTokens(true);
-            stringToWordVector.setWordsToKeep(100);
+
+            doStringToWordVector();
+
+            useStringToWordVector();
+
+            doAtrributeSelection();
+
+            doFilteredClassifier();
 
 
-            fc = new FilteredClassifier();
-            fc.setFilter(stringToWordVector);
-            fc.setClassifier(j48);
-            fc.buildClassifier(trainDataSet);
-            System.out.println(fc);
 
-
-            System.out.println("------------- Train Data Test ------------- \n");
+            System.out.println("\n\n\n\n------------- Train Data Test ------------- \n");
             Evaluation evalTrain =  new Evaluation(trainDataSet);
             evalTrain.evaluateModel(fc,trainDataSet);
 
-            System.out.println(evalTrain.toSummaryString());
-            System.out.println("------------- Test Data Test ------------- \n");
+            printEvaluation(evalTrain);
+
+            System.out.println("\n\n\n\n------------- Test Data Test ------------- \n");
 
             Evaluation evalTest = new Evaluation(testDataSet);
             evalTest.evaluateModel(fc,testDataSet);
-
-            System.out.println(evalTest.toSummaryString());
-
-
-
-
+            printEvaluation(evalTest);
         }catch (Exception e){
-            e.printStackTrace();;
+            e.printStackTrace();
         }
+    }
+
+    private static void useStringToWordVector() throws Exception {
+        trainDataSet = StringToWordVector.useFilter(trainDataSet,stringToWordVector);
+        testDataSet = StringToWordVector.useFilter(testDataSet,stringToWordVector);
+    }
+
+    private static void doFilteredClassifier() throws Exception {
+        fc = new FilteredClassifier();
+        fc.setFilter(aSFilter);
+        fc.setClassifier(j48);
+        fc.buildClassifier(trainDataSet);
+        System.out.println(fc);
+
+    }
+
+    private static void doAtrributeSelection() {
+        aSFilter =new AttributeSelection();
+        aSFilter.setEvaluator(new InfoGainAttributeEval());
+        Ranker ranker = new Ranker();
+        ranker.setThreshold(0.0);
+        ranker.setNumToSelect(NUM_TO_SELECT);
+        aSFilter.setSearch(ranker);
+    }
+
+    private static void doStringToWordVector() throws Exception {
+        stringToWordVector = new StringToWordVector();
+        System.out.println(stringToWordVector.isOutputFormatDefined());
+        stringToWordVector.setOutputWordCounts(true);
+        stringToWordVector.setLowerCaseTokens(true);
+        stringToWordVector.setInputFormat(testDataSet);
+        stringToWordVector.setWordsToKeep(WORDS_TO_KEEP);
+        //stringToWordVector.setTokenizer(new AlphabeticTokenizer());
+    }
+
+    private static void printEvaluation(Evaluation eval) throws Exception {
+        System.out.println(eval.toMatrixString("Matrix:"));
+        System.out.println(eval.toClassDetailsString());
+        System.out.println(eval.toSummaryString());
     }
 }
